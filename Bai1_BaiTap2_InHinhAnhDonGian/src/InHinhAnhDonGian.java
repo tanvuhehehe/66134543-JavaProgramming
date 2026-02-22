64af01403a60c342df0f55357880e5ca1d10774f
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-02-22 21:02:19 +0700

    Hoàn Thành Bài 1 - Bài Tập 2

--- Bai1_BaiTap2_InHinhAnhDonGian/src/InHinhAnhDonGian.java
+++ Bai1_BaiTap2_InHinhAnhDonGian/src/InHinhAnhDonGian.java
@@ -2,10 +2,10 @@
 public class InHinhAnhDonGian {
 
 	public static void main(String[] args) {
-		System.out.println("   *   ");
-	    System.out.println("  ***  ");
-	    System.out.println(" ***** ");
-	    System.out.println("*******");
+	    	System.out.println("   *   ");
+	        System.out.println("  ***  ");
+	        System.out.println(" ***** ");
+	        System.out.println("*******");
 
 	}
 
