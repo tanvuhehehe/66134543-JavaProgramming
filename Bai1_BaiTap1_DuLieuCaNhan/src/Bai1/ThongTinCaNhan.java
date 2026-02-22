6efe5f9699e507c30756dcb5f5e5368dc99b649b
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-02-22 20:47:52 +0700

    dang chinh sua

---
+++ Bai1_BaiTap1_DuLieuCaNhan/src/Bai1/ThongTinCaNhan.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Bai1;
+
+/**
+ * 
+ */
+public class ThongTinCaNhan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
