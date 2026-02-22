805f5395e0dd3bd18d26d3c94954073893ab7853
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-02-22 21:15:55 +0700

    Hoàn Thành Bài 1 - Bài Tập 3

--- Bai1_BaiTap3_TinhToanDonGian/src/TinhToanCoBan.java
+++ Bai1_BaiTap3_TinhToanDonGian/src/TinhToanCoBan.java
@@ -2,7 +2,15 @@
 public class TinhToanCoBan {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 9;
+		int b = 3;
+
+		System.out.println("=== CÁC PHÉP TOÁN CƠ BẢN ===");
+
+		System.out.printf("Cộng: %d + %d = %d\n", a, b, a + b);
+		System.out.printf("Trừ: %d - %d = %d\n", a, b, a - b);
+		System.out.printf("Nhân: %d * %d = %d\n", a, b, a * b);
+		System.out.printf("Chia: %d / %d = %d\n", a, b, a / b);
 
 	}
 
