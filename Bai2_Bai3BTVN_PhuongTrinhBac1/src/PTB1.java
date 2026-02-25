c81e8407b96388eecd0b89884f1203d454fe1815
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-02-25 12:18:18 +0700

    Hoàn Thành  Bài 2 - Bài 3 BTVN

--- Bai2_Bai3BTVN_PhuongTrinhBac1/src/PTB1.java
+++ Bai2_Bai3BTVN_PhuongTrinhBac1/src/PTB1.java
@@ -1,4 +1,18 @@
 
 public class PTB1 {
+	public static void main(String[] args) {
+	       	double a = 2;
+	        double b = -4;
 
+	        if (a == 0) {
+	            if (b == 0) {
+	                System.out.println("Phuong trinh vo so nghiem");
+	            } else {
+	                System.out.println("Phuong trinh vo nghiem");
+	            }
+	        } else {
+	            double x = -b / a;
+	            System.out.println("Nghiem x = " + x);
+	        }
+	}
 }
