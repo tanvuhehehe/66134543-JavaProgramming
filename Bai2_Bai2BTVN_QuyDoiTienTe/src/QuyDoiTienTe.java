14d863b6b366e46e1deb425ab3aa7b889c431f8f
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-02-25 12:09:52 +0700

    Hoàn Thành Bài 2 - Bài2BTVN

--- Bai2_Bai2BTVN_QuyDoiTienTe/src/QuyDoiTienTe.java
+++ Bai2_Bai2BTVN_QuyDoiTienTe/src/QuyDoiTienTe.java
@@ -4,8 +4,10 @@ public class QuyDoiTienTe {
 	public static void main(String[] args) {
 			int usd = 100;
 	        int eur = 100;
+	        
 	        final int TY_GIA_USD = 23500;
 	        final int TY_GIA_EUR = 27000;
+	        
 	        int vndFromUSD = usd * TY_GIA_USD;
 	        int vndFromEUR = eur * TY_GIA_EUR;
 
