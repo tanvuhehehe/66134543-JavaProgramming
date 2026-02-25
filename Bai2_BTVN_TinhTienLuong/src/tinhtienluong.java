b710f2b636a6693643ad80017d864bf3880dfd2c
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-02-25 11:24:46 +0700

    Hoàn Thành Bài 1 bài tập về nhà

--- Bai2_BTVN_TinhTienLuong/src/tinhtienluong.java
+++ Bai2_BTVN_TinhTienLuong/src/tinhtienluong.java
@@ -2,8 +2,18 @@
 public class tinhtienluong {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			double soGio = 45;        
+	        double luongMoiGio = 50000; 
+	        double tongLuong;
 
+	        if (soGio > 40) {
+	            tongLuong = 40 * luongMoiGio 
+	                      + (soGio - 40) * luongMoiGio * 1.5;
+	        } else {
+	            tongLuong = soGio * luongMoiGio;
+	        }
+
+	        System.out.println("Tong luong nhan vien: " + tongLuong);
 	}
 
 }
