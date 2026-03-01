80409b23ae221b8c2b6d089fcb6c8a47239d66c3
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-01 15:25:47 +0700

    Xong 2

--- LuyenTapThem_2_KiemTraManHinhCoPhaiSoLeHoacSoChanHayKhongVaInRa/src/checklevachan.java
+++ LuyenTapThem_2_KiemTraManHinhCoPhaiSoLeHoacSoChanHayKhongVaInRa/src/checklevachan.java
@@ -1,9 +1,27 @@
 
+
+	/*Đề Bài: Viết chương trình có sẳn số n, kiểm tra số nhập vào có phải là số chẵn không, nếu là số chẵn in ra màn hình là "So chan", 
+	 ngược lại in ra màn hình số lẻ "So le".
+
+	Ví dụ
+
+	input: 21
+
+	ouput: So le
+
+	input: 300
+
+	output: So chan */
 public class checklevachan {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    	int n = 21;   
 
+	     	if (n % 2 == 0) {
+	            System.out.println("So " + n + " la so chan");
+	        } else {
+	            System.out.println("So " + n + " la so le");
+	        }
 	}
 
 }
