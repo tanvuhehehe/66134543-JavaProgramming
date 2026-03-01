bcfd66165cee956f10b95c265ed0af1b8ac19634
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-01 15:16:50 +0700

    Xong 1 làm thêm

--- LuyenTapThem_1_InRaPhepTinhCongTruNhanChia/src/congtrunhanchia.java
+++ LuyenTapThem_1_InRaPhepTinhCongTruNhanChia/src/congtrunhanchia.java
@@ -2,8 +2,15 @@
 public class congtrunhanchia {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+        int x = 20;
+        int y = 10;
+
+        System.out.println("Cộng: " + x + " + " + y + " = " + (x + y));
+        System.out.println("Trừ: " + x + " - " + y + " = " + (x - y));
+        System.out.println("Nhân: " + x + " * " + y + " = " + (x * y));
+        System.out.printf("Chia: %d / %d = %.2f\n", x, y, (double)x / y);
+        System.out.println("Phần dư: " + x + " % " + y + " = " + (x % y));
 	}
 
 }
