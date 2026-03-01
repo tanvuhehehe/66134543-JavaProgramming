74eac520fcb661b1fbd9ef9b5cf587eae4629929
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-01 15:30:52 +0700

    xong 3

--- LuyenTapThem_3_InRaSoLonNhatTrongHaiSoab/src/inrasolonnhat.java
+++ LuyenTapThem_3_InRaSoLonNhatTrongHaiSoab/src/inrasolonnhat.java
@@ -1,9 +1,33 @@
+/*Cho 2 số nguyên a và b từ bàn phím, in ra số lớn nhất ?
+
+Ví dụ
+
+input: 10 20 30
+
+output:30 
+
+input:100 200 500
+
+output:500*/
 
 public class inrasolonnhat {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int a = 10;
+        int b = 20;
+        int c = 30;
 
-	}
+        int max = a;  // giả sử a là lớn nhất
 
+        if (b > max) {
+            max = b;
+        }
+
+        if (c > max) {
+            max = c;
+        }
+
+        System.out.println("So lon nhat la: " + max);
+    }
 }
+
