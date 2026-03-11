f728476ba9d8cf15ba38a636ca9977d1a8de534d
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-11 22:33:36 +0700

    đang tạo và làm bài rectangle

--- Ex_Circle_1_3/src/mainrecrangle.java
+++ Ex_Circle_1_3/src/mainrecrangle.java
@@ -2,8 +2,19 @@
 public class mainrecrangle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Rectangle r1 = new Rectangle(2.2f, 6.4f);
+	    System.out.println(r1);  
+	    Rectangle r2 = new Rectangle();  
+	    System.out.println(r2);
+	    
+	    r1.setLength(9.6f);
+	    r1.setWidth(12.8f);
+	    System.out.println(r1);  
+	    System.out.println("Chieu dai: " + r1.getLength());
+	    System.out.println("Chieu rong: " + r1.getWidth());
+	    
+	    System.out.printf("Dien tich: %.2f%n", r1.getArea());
+	    System.out.printf("Chu vi: %.2f%n", r1.getPerimeter());
 	}
 
 }
