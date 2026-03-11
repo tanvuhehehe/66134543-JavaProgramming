b316ab0fc1774474b1407b8923b7dd152a788d65
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-11 22:30:53 +0700

    đang làm rectanggele mà chưa đi tắm nên giờ em đi tắm xong làm tiếp

--- Ex_Circle_1_3/src/Rectangle.java
+++ Ex_Circle_1_3/src/Rectangle.java
@@ -1,4 +1,26 @@
 
 public final class Rectangle {
-
+	private float length;
+	private float width;
+	
+	public Rectangle() {
+		length = 1.0f;
+		width = 1.0f;
+	}
+	public Rectangle(float length, float width) {
+		this.length = length;
+		this.width = width;
+	}
+	public float getLength() {
+		return length;
+	}
+	public float getWidth() {
+		return width;
+	}
+	public void setLength(float l) {
+		length = l;
+	}
+	public void setWidth(float w) {
+		width = w;
+	}
 }
