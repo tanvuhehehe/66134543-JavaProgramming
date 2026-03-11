4c789c714a4684f3ed6868fcf4553ff32a05f2d8
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-11 22:42:39 +0700

    đã làm xong hàm getter và chuẩn bị tạo hàm main chính

--- Bai6_OOP_Vidu2_HamGettervaSetting/src/ChuNhat.java
+++ Bai6_OOP_Vidu2_HamGettervaSetting/src/ChuNhat.java
@@ -1,4 +1,33 @@
 
 public class ChuNhat {
+	private double dai;
+	private double rong;
 
+	public ChuNhat() {
+		dai = 10;
+		rong = 5;
+	}
+	public ChuNhat(double dai, double rong) {
+		this.dai = dai;
+		this.rong = rong;
+	}
+
+	double getDai() {
+		return dai;
+	}
+	double getRong() {
+		return rong;
+	}
+
+	void setDai(double dai) {
+		this.dai = dai;
+	}
+	void setRong(double rong) {
+		this.rong = rong;
+	}	
+
+	public String toString() {
+		String s = "Hinh co chieu dai la " + dai + " va chieu rong la " + rong;
+		return s;
+	}
 }
