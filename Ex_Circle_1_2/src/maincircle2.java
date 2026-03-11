b17cdd91a1ccebe092c3c38f955461fa9cdb6f34
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-11 22:25:22 +0700

    xong hàm main circle là xong bài excicle1_2

--- Ex_Circle_1_2/src/maincircle2.java
+++ Ex_Circle_1_2/src/maincircle2.java
@@ -2,8 +2,17 @@
 public class maincircle2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		circle c1 = new circle(3.3);
+	    System.out.println(c1);   
+	    circle c2 = new circle();
+	    System.out.println(c2);
+	    
+	    c1.setRadius(2.2);
+	    System.out.println(c1);      
+	    System.out.println("Ban kinh: " + c1.getRadius());
+	    
+	    System.out.printf("Dien tich: %.2f%n", c1.getArea());
+	    System.out.printf("Chu vi: %.2f%n", c1.getCircumference());
 	}
 
 }
