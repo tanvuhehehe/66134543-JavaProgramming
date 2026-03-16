e8c8c476ef91bd2ed2ba1cd2887f552a00e97907
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-16 17:29:51 +0700

    đã code xong bài nhập thông tin bản thân cơ bản là nhập tên, năm sinh, nhành học

--- LuyenTapThem_4_NhapXuatInRaThongTinCaNhan/src/inthongtincanhan.java
+++ LuyenTapThem_4_NhapXuatInRaThongTinCaNhan/src/inthongtincanhan.java
@@ -1,8 +1,21 @@
-
+import java.util.Scanner;	
 public class inthongtincanhan {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner nhap = new Scanner(System.in);
+		
+		System.out.print("Nhap  ten: ");
+		String name = nhap.nextLine();
+		
+		System.out.print("Nhap nam sinh: ");
+		int age = nhap.nextInt();
+		nhap.nextLine(); 
+		System.out.print("Nhap Nganh Dang Hoc");
+		String nganh = nhap.nextLine();
+		System.out.println();
+		System.out.println("Ten: " + name);
+		System.out.println("Tuoi: " + age);
+		System.out.println("Khoa dang hoc: " + nganh);
 
 	}
 
