e468c99b6088b536230ae9e3a7fc9d318af20268
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-17 16:57:27 +0700

    Chỉnh sửa lỗi chính tả

--- LuyenTapThem_6_OOP_XeHoiDonGian/src/mainchinhxehoi.java
+++ LuyenTapThem_6_OOP_XeHoiDonGian/src/mainchinhxehoi.java
@@ -4,19 +4,19 @@ public class mainchinhxehoi {
 	public static void main(String[] args) {
 	       xehoi xe1 = new xehoi();
 	        xe1.hangXe = "Toyota";
-	        xe1.mauSac = "Đo";
+	        xe1.mauSac = "Do";
 	        xe1.tocDoHienTai = 50;
 	        
 	        xehoi xe2 = new xehoi();
 	        xe2.hangXe = "Honda";
-	        xe2.mauSac = "Trắng";
+	        xe2.mauSac = "Trang";
 	        xe2.tocDoHienTai = 0;
 	        
-	        System.out.println(" THÔNG TIN XE 1 ");
+	        System.out.println(" THONG TIN XE 1 ");
 	        xe1.hienThiThongTin();
 	        xe1.tangToc(30);
 	        
-	        System.out.println("\n THÔNG TIN XE 2 ");
+	        System.out.println("\n THONG TIN XE 2 ");
 	        xe2.hienThiThongTin();
 	        xe2.tangToc(60);
 	        xe2.phanh();
