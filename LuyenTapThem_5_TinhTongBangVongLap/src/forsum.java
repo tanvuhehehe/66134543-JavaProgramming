127c37c25b49ca14ccab8b19dc857eab34bfcb82
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-17 10:11:12 +0700

    đang làm dowhile

--- LuyenTapThem_5_TinhTongBangVongLap/src/forsum.java
+++ LuyenTapThem_5_TinhTongBangVongLap/src/forsum.java
@@ -5,6 +5,22 @@ public class forsum {
 		Scanner nhap = new Scanner(System.in);
 		System.out.print("Nhap n: ");
 		int n = nhap.nextInt();
+		int sumfor = 0;
+		for(int i = 0; i < n; i++) {
+			sumfor += i;
+		}
+		System.out.println("Tong tu 1 den n la: " + sumfor);
+		
+		int sumwhile = 0;
+		int i = 1;
+		while(i < n) {
+			sumwhile += i;
+			i++;
+		}
+		System.out.println("Tong tinh bang while: " + sumwhile);
+		
+		
+		
 		
 	}
 
