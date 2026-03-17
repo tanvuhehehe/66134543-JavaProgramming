519c4d56106a331712b3983ca339a0b601387a04
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-17 10:10:14 +0700

    đang làm tới tính tổng while

--- LuyenTapThem_5_TinhTongBangVongLap/src/forsum.java
+++ LuyenTapThem_5_TinhTongBangVongLap/src/forsum.java
@@ -1,9 +1,11 @@
-
+import java.util.Scanner;
 public class forsum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner nhap = new Scanner(System.in);
+		System.out.print("Nhap n: ");
+		int n = nhap.nextInt();
+		
 	}
 
 }
