c0997888fcbcd706a4c54d4898f608e93c92dc97
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-17 16:56:49 +0700

    xong bài xe hơi, class main chính, class xe hơi cũng xong

--- LuyenTapThem_6_OOP_XeHoiDonGian/src/mainchinhxehoi.java
+++ LuyenTapThem_6_OOP_XeHoiDonGian/src/mainchinhxehoi.java
@@ -2,7 +2,25 @@
 public class mainchinhxehoi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	       xehoi xe1 = new xehoi();
+	        xe1.hangXe = "Toyota";
+	        xe1.mauSac = "Đo";
+	        xe1.tocDoHienTai = 50;
+	        
+	        xehoi xe2 = new xehoi();
+	        xe2.hangXe = "Honda";
+	        xe2.mauSac = "Trắng";
+	        xe2.tocDoHienTai = 0;
+	        
+	        System.out.println(" THÔNG TIN XE 1 ");
+	        xe1.hienThiThongTin();
+	        xe1.tangToc(30);
+	        
+	        System.out.println("\n THÔNG TIN XE 2 ");
+	        xe2.hienThiThongTin();
+	        xe2.tangToc(60);
+	        xe2.phanh();
+	        xe1.phanh();
 
 	}
 
--- LuyenTapThem_6_OOP_XeHoiDonGian/src/xehoi.java
+++ LuyenTapThem_6_OOP_XeHoiDonGian/src/xehoi.java
@@ -1,4 +1,21 @@
 
 public class xehoi {
-
+	// thuoc tinh
+	String hangXe;
+	String mauSac;
+	int tocDoHienTai;
+	// phuong thuc hanh dong
+	void tangToc(int km) {
+		tocDoHienTai += km;
+		System.out.println(hangXe + " tang toc len " + tocDoHienTai + "km/h");
+	}
+    void phanh() {
+        tocDoHienTai = 0;
+        System.out.println(hangXe + " da dung lai");
+    }
+    void hienThiThongTin() {
+        System.out.println("Hang xe: " + hangXe);
+        System.out.println("Mau sac: " + mauSac);
+        System.out.println("Toc do hien tai: " + tocDoHienTai + " km/h");
+    }
 }
