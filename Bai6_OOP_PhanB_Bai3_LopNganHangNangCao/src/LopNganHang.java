77b8d5aa7019dd844bcd77352a5ca376678c9f22
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-22 16:54:01 +0700

    đang làm phương thức rút tiền

--- Bai6_OOP_PhanB_Bai3_LopNganHangNangCao/src/LopNganHang.java
+++ Bai6_OOP_PhanB_Bai3_LopNganHangNangCao/src/LopNganHang.java
@@ -1,4 +1,21 @@
 
 public class LopNganHang {
-
+	String soTaiKhoan;
+    String tenChuTaiKhoan;
+    double soDu;
+    
+    void guiTien(double soTien) {
+    	soDu += soTien;
+    }
+    
+    
+    boolean rutTien(double soTien) {
+    	if(soDu <= soTien) {
+    		soDu -= soTien;
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
+    }
 }
