8b1fa05c04b7fb677103c685b26f82108270ad4e
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-22 14:24:17 +0700

    xong phuong thuc 2

--- Bai6_OOP_PhanB_Bai1_LopSachCoBan/src/Mainchinh.java
+++ Bai6_OOP_PhanB_Bai1_LopSachCoBan/src/Mainchinh.java
@@ -2,12 +2,27 @@
 public class Mainchinh {
 
 	public static void main(String[] args) {
+		
+		// doi tuong 1	
 		hamsach sach1 = new hamsach();
-		sach1.tieuDe = "Co So Du Lieu";
+		sach1.tieuDe = "He Quan Tri Co So Du Lieu";
 		sach1.tacGia = "Pham Thi Thu Thuy";
 		sach1.Gia = 68000;
 		sach1.soTrang = 300;
 		sach1.hienThiThongTin();
+		
+		System.out.println("\n\n\n");
+		// doi tuong 2
+		hamsach sach2 = new hamsach();
+		sach2.tieuDe = "Lap Trinh Web";
+		sach2.tacGia = "Nguyen Dinh Hoang Son";
+		sach2.Gia = 300000;
+		sach2.soTrang = 400;
+		sach2.hienThiThongTin();
+		
+		// doi tuong 3
+		
+
 	}
 
 }
