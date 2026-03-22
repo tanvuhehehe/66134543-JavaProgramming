c12098919384fca8557da9b87712ef88c0211ab5
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-22 17:28:10 +0700

    làm xong lớp con mèo

--- Bai6_OOP_PhanC_ThuThacSangTaoConMeoKeu/src/conmeokeusao.java
+++ Bai6_OOP_PhanC_ThuThacSangTaoConMeoKeu/src/conmeokeusao.java
@@ -6,13 +6,19 @@ public class conmeokeusao {
 	String mauLong;
 	double canNang;
 	//phuongthuc
+	void HienThongTin() {
+		System.out.println("Ten con meo: " + tenConMeo);
+		System.out.println("Tuoi con Meo: " + tuoiMeo);
+		System.out.println("Mau Long:  " + mauLong);
+		System.out.println("Can Nang: " + canNang);
+	}
 	void keu() {
 		System.out.println(tenConMeo +" kêu Mèo méo meo mèo meo ");
 	}
 	void an(String thucAn) {
-		System.out.println(ten + " dang an " + thucAn);
+		System.out.println(tenConMeo + " dang an " + thucAn);
 	}
 	void ngu() {
-		System.out.println(ten + " dang ngu");
+		System.out.println(tenConMeo + " dang ngu");
 	}
 }
