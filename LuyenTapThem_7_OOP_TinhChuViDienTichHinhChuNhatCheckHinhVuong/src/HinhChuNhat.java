4eea506617a2665263ab22c96c235b96fec62fcf
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-22 10:48:37 +0700

    thêm hàm check hợp lệ

--- LuyenTapThem_7_OOP_TinhChuViDienTichHinhChuNhatCheckHinhVuong/src/HinhChuNhat.java
+++ LuyenTapThem_7_OOP_TinhChuViDienTichHinhChuNhatCheckHinhVuong/src/HinhChuNhat.java
@@ -2,19 +2,21 @@
 public class HinhChuNhat {
 	double chieudai;
 	double chieurong;
-	if(chieudai < chieurong) {
-		double tam;
-		tam = chieudai;
-		chieudai = chieurong;
-		chieurong = tam;
-	}
+	
 	double tinhdientich() {
 		return chieudai * chieurong;
 	}
+	
 	double tinhchuvi() {
 		return (chieudai + chieurong) * 2;
 	}
+	
+	boolean hople() {
+	    return chieudai > 0 && chieurong > 0;
+	}
+	
 	void hienthongtin() {
+		System.out.println("Du kien hop le: " + (hople() ? "Co" : "Khong"));
 		System.out.println("Chieu dai cua ban nhap la: " + chieudai +" cm");
 		System.out.println("Chieu rong cua ban nhap la: " + chieurong + " cm");
 		System.out.println("Dien tich cua hinh chu nhat la: " + tinhdientich() + " cm^2");
