b910a78197d6a0fbf0d36242cd3ae5f10a31afe0
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-22 10:34:04 +0700

    làm xong classs hình chữ nhật

--- LuyenTapThem_7_OOP_TinhChuViDienTichHinhChuNhatCheckHinhVuong/src/HinhChuNhat.java
+++ LuyenTapThem_7_OOP_TinhChuViDienTichHinhChuNhatCheckHinhVuong/src/HinhChuNhat.java
@@ -2,9 +2,22 @@
 public class HinhChuNhat {
 	double chieudai;
 	double chieurong;
-	
+	if(chieudai < chieurong) {
+		double tam;
+		tam = chieudai;
+		chieudai = chieurong;
+		chieurong = tam;
+	}
 	double tinhdientich() {
-		return chieudai*chieurong;
-		
+		return chieudai * chieurong;
+	}
+	double tinhchuvi() {
+		return (chieudai + chieurong) * 2;
+	}
+	void hienthongtin() {
+		System.out.println("Chieu dai cua ban nhap la: " + chieudai +" cm");
+		System.out.println("Chieu rong cua ban nhap la: " + chieurong + " cm");
+		System.out.println("Dien tich cua hinh chu nhat la: " + tinhdientich() + " cm^2");
+		System.out.println("Chu vi cua hinh chu nhat la: " + tinhchuvi() + " cm");
 	}
 }
