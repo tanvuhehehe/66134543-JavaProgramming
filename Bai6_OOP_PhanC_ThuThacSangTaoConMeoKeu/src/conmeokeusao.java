4f3de24eab0e265e1f6403d9ad253bd37c404451
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-22 17:21:32 +0700

    đang làm con mèo kêu sao

--- Bai6_OOP_PhanC_ThuThacSangTaoConMeoKeu/src/conmeokeusao.java
+++ Bai6_OOP_PhanC_ThuThacSangTaoConMeoKeu/src/conmeokeusao.java
@@ -1,4 +1,14 @@
 
 public class conmeokeusao {
-
+	// thuoc tinh
+	String tenConMeo;
+	int tuoiMeo;
+	String mauLong;
+	double canNang;
+	//phuongthuc
+	void keu() {
+		System.out.println(tenConMeo +" kêu Mèo méo meo mèo meo ");
+	}
+	void an()
+	
 }
