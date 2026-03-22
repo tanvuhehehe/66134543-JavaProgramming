566df4b8749d9044999ba11a9c16355a92af8019
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-22 14:21:06 +0700

    xong phuowng thuc thu 1

---
+++ Bai6_OOP_PhanB_Bai1_LopSachCoBan/src/Mainchinh.java
@@ -0,0 +1,13 @@
+
+public class Mainchinh {
+
+	public static void main(String[] args) {
+		hamsach sach1 = new hamsach();
+		sach1.tieuDe = "Co So Du Lieu";
+		sach1.tacGia = "Pham Thi Thu Thuy";
+		sach1.Gia = 68000;
+		sach1.soTrang = 300;
+		sach1.hienThiThongTin();
+	}
+
+}
