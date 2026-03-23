56c2e7ebb514e1b00c788136adc083443257fff6
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-03-23 17:11:57 +0700

    đang làm bài

--- Bai7_PhanB_BaiTapThucHanh_Bai1_LopSanPhamVoiEncapsulation/src/LopSanPham.java
+++ Bai7_PhanB_BaiTapThucHanh_Bai1_LopSanPhamVoiEncapsulation/src/LopSanPham.java
@@ -1,5 +1,18 @@
 
 public class LopSanPham {
-	// private: maSP, tenSP, gia, soLuong
+	// 1. private: maSP, tenSP, gia, soLuong
+		private String maSP;
+		private String tenSP;
+		private double gia;
+		private int soLuong;
+		
+	// 2. 2 constructor: mặc định và có tham số
+		public SanPham ( String maSP, String tenSP, double  gia, int soLuong) {
+			this.maSP = "1111";
+			this.tenSP = "Chua co ten nha !";
+			this.gia = 0;
+			this.soLuong = 0;
+		}
+	// consstructor co tham so
 		
 }
