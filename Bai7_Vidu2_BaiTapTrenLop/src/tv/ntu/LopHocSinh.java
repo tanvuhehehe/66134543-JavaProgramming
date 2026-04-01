e1932569e897e251b2f097dca3d852f2635e118e
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-01 21:58:47 +0700

    tao file rong + lam lop hoc sinh

---
+++ Bai7_Vidu2_BaiTapTrenLop/src/tv/ntu/LopHocSinh.java
@@ -0,0 +1,37 @@
+package tv.ntu;
+
+public class LopHocSinh {
+	private String tenHocSinh;
+	private int tuoiHocSinh;
+	private String lopHocSinh;
+	
+	public String getTenHocSinh() {
+		return tenHocSinh;
+	}
+	public void setTenHocSinh(String tenHocSinh) {
+		this.tenHocSinh = tenHocSinh;
+	}
+	public int getTuoiHocSinh() {
+		return tuoiHocSinh;
+	}
+	public void setTuoiHocSinh(int tuoiHocSinh) {
+		this.tuoiHocSinh = tuoiHocSinh;
+	}
+	public String getLopHocSinh() {
+		return lopHocSinh;
+	}
+	public void setLopHocSinh(String lopHocSinh) {
+		this.lopHocSinh = lopHocSinh;
+	}
+	
+	public LopHocSinh(){
+		
+	}
+	@Override
+	public String toString() {
+		return "LopHocSinh [tenHocSinh=" + tenHocSinh + ", tuoiHocSinh=" + tuoiHocSinh + ", lopHocSinh=" + lopHocSinh
+				+ "]";
+	}
+	
+	
+}
