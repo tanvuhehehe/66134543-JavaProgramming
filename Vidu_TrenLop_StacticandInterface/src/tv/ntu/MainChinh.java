aab8f6d43b6ea852645aefb6d46f2406816f9bd1
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-07 20:39:04 +0700

    đang tạo run MAIn

---
+++ Vidu_TrenLop_StacticandInterface/src/tv/ntu/MainChinh.java
@@ -0,0 +1,14 @@
+package tv.ntu;
+
+public class MainChinh {
+
+	public static void main(String[] args) {
+		System.out.println(SinhVien.TEN_TRUONG);
+		SinhVien.sayHi();
+
+		sv1.sayHello();
+		sv2.sayHello();
+
+	}
+
+}
