481c51f7dc28aa18efc807f47577c5833b96beec
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-07 20:45:30 +0700

    ĐÃ LÀM XONG VÍ DỤ TRÊN LỚP

--- Vidu_TrenLop_StacticandInterface/src/tv/ntu/MainChinh.java
+++ Vidu_TrenLop_StacticandInterface/src/tv/ntu/MainChinh.java
@@ -5,8 +5,12 @@ public class MainChinh {
 	public static void main(String[] args) {
 		System.out.println(SinhVien.TEN_TRUONG);
 		SinhVien.sayHi();
-
+		System.out.println();
+		SinhVien sv1 = new SinhVien("Huỳnh Tấn Vũ", "CNTT3");
+		SinhVien sv2 = new SinhVien("Nguyễn Ngọc Lan Anh", "CNTT100");
+		
 		sv1.sayHello();
+		System.out.println();
 		sv2.sayHello();
 
 	}
--- Vidu_TrenLop_StacticandInterface/src/tv/ntu/SinhVien.java
+++ Vidu_TrenLop_StacticandInterface/src/tv/ntu/SinhVien.java
@@ -1,5 +1,33 @@
 package tv.ntu;
 
 public class SinhVien {
+	private String hoTen;
+	private String lop;
+	public String getHoTen() {
+		return hoTen;
+	}
+	public void setHoTen(String hoTen) {
+		this.hoTen = hoTen;
+	}
+	public String getLop() {
+		return lop;
+	}
+	public void setLop(String lop) {
+		this.lop = lop;
+	}
+	//  static : tĩnh
+	public static String TEN_TRUONG = "ĐẠI HỌC NHA TRANG";
+	
+	public static void sayHi() {
+		System.out.println("Hi, I am a NTU student!");
+	}
+	
+	public void sayHello() {
+		System.out.println("Hellooooooo, I am a NTU student: \nName: " + hoTen + "\nClass: " + lop);
+	}
+	public SinhVien(String hoTen, String lop) {
 
+		this.hoTen = hoTen;
+		this.lop = lop;
+	}
 }
