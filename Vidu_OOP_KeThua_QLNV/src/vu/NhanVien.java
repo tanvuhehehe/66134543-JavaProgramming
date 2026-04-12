df74e74b1553e387b2f2836101180b3aadf3a700
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-12 23:20:30 +0700

    buồn ngủ

--- Vidu_OOP_KeThua_QLNV/src/vu/NhanVien.java
+++ Vidu_OOP_KeThua_QLNV/src/vu/NhanVien.java
@@ -1,5 +1,40 @@
 package vu;
 
 public class NhanVien {
+	//thuoc tinh
+	private String maNV;
+	private String hoTen;
+	private double luongCoBan;
+	//getter setter
+	
+	// Ham tao
+	public NhanVien() {
+		
+	}
+	
+	public String getMaNV() {
+		return maNV;
+	}
+	
+	public void setMaNV(String maNV) {
+		this.maNV = maNV;
+	}
+	public String getHoTen() {
+		return hoTen;
+	}
+	public void setHoTen(String hoTen) {
+		this.hoTen = hoTen;
+	}
+	public double getLuongCoBan() {
+		return luongCoBan;
+	}
+	public void setLuongCoBan(double luongCoBan) {
+		this.luongCoBan = luongCoBan;
+	}
+	public NhanVien(String maNV, String hoTen, double luongCoBan) {
+		this.maNV = maNV;
+		this.hoTen = hoTen;
+		this.luongCoBan = luongCoBan;
+	}
 
 }
