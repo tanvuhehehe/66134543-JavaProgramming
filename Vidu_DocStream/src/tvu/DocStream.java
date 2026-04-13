98c02b9d1b12dfdf09180b89a1c9266d86c4d58e
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-13 15:46:33 +0700

    Hoàn Thành phần đọc dữ liệu từ file + từ bàn phím ko qua lớp scanner

--- Vidu_DocStream/src/tvu/DocStream.java
+++ Vidu_DocStream/src/tvu/DocStream.java
@@ -1,6 +1,7 @@
 package tvu;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -8,9 +9,11 @@ public class DocStream {
 
 	public static void main(String[] args) throws IOException {
 		
-		//Khai báo biến đối tượng InputStreamReader
-		InputStreamReader ipReader = new InputStreamReader(System.in);
+		FileInputStream fInput = new FileInputStream("baitho.txt");
 		
+		//Khai báo biến đối tượng InputStreamReader
+//		InputStreamReader ipReader = new InputStreamReader(System.in);
+		InputStreamReader ipReader = new InputStreamReader(fInput);
 		//Khai báo biến đối tượng buffer 
 		BufferedReader bufReader = new BufferedReader(ipReader);
 		
@@ -22,6 +25,9 @@ public class DocStream {
 		
 		//In ra dòng đọc được
 		System.out.print(lineDocDuoc);
+		System.out.println();
+		String lineDocDuoc1 = bufReader.readLine(); 
+		System.out.print(lineDocDuoc1);
 		
 		
 	}
