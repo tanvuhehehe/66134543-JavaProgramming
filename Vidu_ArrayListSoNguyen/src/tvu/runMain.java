d70ae72136cc4e77a753198ca70b240e41d8ffea
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-13 13:45:54 +0700

    tạo xong chổ nhập rồi

--- Vidu_ArrayListSoNguyen/src/tvu/runMain.java
+++ Vidu_ArrayListSoNguyen/src/tvu/runMain.java
@@ -1,10 +1,56 @@
+
+/* De bai: Dung Arraylist
+ * Viet chuong trinh
+ * a. Nhap vao 10 so nguyen tu ban phim
+ * b. In ra man hinh day so vua nhap
+ * c. Dem xem co bao nhieu so chan
+ * d. Tinh tong cac phan tu co trong danh sach
+ */
 package tvu;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 public class runMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<Integer> ds = new ArrayList<Integer>();
+		//a.Nhap vao 10 so nguyen tu ban phim
+		Scanner sc = new Scanner(System.in);
+		for(int i = 0; i < 10; i++) {
+//			System.out.println("Nhap phan tu thu " + i + " : ");
+			System.out.println("a[" + i + "] = ");
+			Integer tam = sc.nextInt();
+			ds.add(tam);
+		}
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
