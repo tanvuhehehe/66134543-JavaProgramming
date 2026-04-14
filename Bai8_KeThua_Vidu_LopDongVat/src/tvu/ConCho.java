3681a34db5c75e522cd71b28b62f1ea75b47ab1e
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-14 22:27:00 +0700

    Làm xong lớp con Chó

--- Bai8_KeThua_Vidu_LopDongVat/src/tvu/ConCho.java
+++ Bai8_KeThua_Vidu_LopDongVat/src/tvu/ConCho.java
@@ -1,5 +1,27 @@
 package tvu;
 
-public class ConCho {
-
+public class ConCho extends DongVat{
+	private String giongCho;
+	private boolean coTheTrongNha;
+	
+	//constructor 
+	public ConCho(String ten, int tuoi, double canNang, 
+			String giongCho, boolean coTheTrongNha) {
+		super(ten, tuoi, canNang);
+		this.giongCho = giongCho;
+		this.coTheTrongNha = coTheTrongNha;
+	}
+    public void sua() {
+        System.out.println(getTen() + " dang sua: Gau Gau !");
+    }
+    public void duoiMeo() {
+        System.out.println(getTen() + " dang duoi Meo");
+    }
+    //phuong thuc
+    @Override
+    public void hienThiThongTin() {
+    		super.hienThiThongTin();
+    		System.out.println("Giong Cho: " + giongCho);
+    		System.out.println("Co the trong nha: " + (coTheTrongNha ? "Co" : "khong"));
+    }
 }
