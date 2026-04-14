faeb37ce519e8478c78bcbc6150482f53dc1f414
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-14 22:34:04 +0700

    đã xong bài lớp động vật

--- Bai8_KeThua_Vidu_LopDongVat/src/tvu/ConMeo.java
+++ Bai8_KeThua_Vidu_LopDongVat/src/tvu/ConMeo.java
@@ -1,5 +1,34 @@
 package tvu;
 
-public class ConMeo {
-
+public class ConMeo extends DongVat {
+	 private String mauLong;
+	 private boolean thichLeoTree;
+	    
+	 public ConMeo(String ten, int tuoi, double canNang, 
+	                  String mauLong, boolean thichLeoTree) {
+	 		super(ten, tuoi, canNang);
+	        this.mauLong = mauLong;
+	        this.thichLeoTree = thichLeoTree;
+	    }
+	 
+	 public void batChuot() {
+	        System.out.println(getTen() + " đang rình bắt chuột...");
+	    }
+	    
+	    public void keu() {
+	        System.out.println(getTen() + " kêu: Meo meo!");
+	    }
+	    
+	
+	    @Override
+	    public void an() {
+	        System.out.println(getTen() + " đang ăn cá một cách nhẹ nhàng...");
+	    }
+	    
+	    @Override
+	    public void hienThiThongTin() {
+	        super.hienThiThongTin();
+	        System.out.println("Màu lông: " + mauLong);
+	        System.out.println("Thích leo trèo: " + (thichLeoTree ? "Có" : "Không"));
+	    }
 }
--- Bai8_KeThua_Vidu_LopDongVat/src/tvu/MainDongVat.java
+++ Bai8_KeThua_Vidu_LopDongVat/src/tvu/MainDongVat.java
@@ -3,8 +3,27 @@ package tvu;
 public class MainDongVat {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ConCho cho1 = new ConCho("Lucky", 3, 15.5, "Husky", true);
+        System.out.println("THÔNG TIN CHÓ:");
+        cho1.hienThiThongTin();
+        cho1.an();  // Kế thừa từ DongVat
+        cho1.sua();  // Phương thức riêng
+        cho1.duoiMeo();
 
+        System.out.println();
+        
+     // Tạo đối tượng Mèo
+        ConMeo meo1 = new ConMeo("Mimi", 2, 4.2, "Tam thể", true);
+        System.out.println("THÔNG TIN MÈO:");
+        meo1.hienThiThongTin();
+        meo1.an();  // Phương thức đã ghi đè
+        meo1.ngu();  // Kế thừa từ DongVat
+        meo1.keu();
+        meo1.batChuot();
+        
+        System.out.println();
+        
+        
 	}
 
 }
