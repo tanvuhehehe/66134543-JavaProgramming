80639e11f888ba419ddf047c565758e523f9621c
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-14 14:25:51 +0700

    đã làm xong lớp DongVat

--- Bai8_KeThua_Vidu_LopDongVat/src/tvu/DongVat.java
+++ Bai8_KeThua_Vidu_LopDongVat/src/tvu/DongVat.java
@@ -1,5 +1,80 @@
 package tvu;
 
 public class DongVat {
-
+	private String ten;
+	private int tuoi;
+	private double canNang;
+	
+	//constructor null
+	public DongVat(){
+		
+	}
+	
+	//constructor co gia tri
+	public DongVat(String ten, int tuoi, double canNang) {
+		this.ten = ten;
+		this.tuoi = tuoi;
+		this.canNang = canNang;
+	}
+	
+	//setter
+	public void setTen(String ten) {
+		this.ten = ten;
+	}
+	public void setTuoi(int tuoi) {
+		this.tuoi = tuoi;
+	}
+	public void setCanNang(double canNang) {
+		this.canNang = canNang;
+	}
+	
+	//getter
+	public String getTen() {
+		return ten;
+	}
+	public int getTuoi() {
+		return tuoi;
+	}
+	public double getCanNang() {
+		return canNang;
+	}
+	//phuong thuc 
+	public void an() {
+		System.out.println(ten + " dang an");
+	}
+	
+	public void ngu() {
+		System.out.println(ten + " dang ngu");
+	}
+	
+	public void hienThiThongTin() {
+		System.out.println("Ten : " + ten);
+		System.out.println("Tuoi: " + tuoi + "nam");
+		System.out.println("Can Nang: " + canNang + "kg");
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
