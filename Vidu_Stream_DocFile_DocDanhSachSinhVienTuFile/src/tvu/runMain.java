2f88e419e88907e24833848670bc4a7689d62d78
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-16 22:44:09 +0700

    đang làm phần reader file ạ

--- Vidu_Stream_DocFile_DocDanhSachSinhVienTuFile/src/tvu/runMain.java
+++ Vidu_Stream_DocFile_DocDanhSachSinhVienTuFile/src/tvu/runMain.java
@@ -1,8 +1,14 @@
 package tvu;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class runMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		FileReader fR = new FileReader("SV.csv");
+		BufferedReader bufReader = new BufferedReader(fR);
 		
 
 	}
