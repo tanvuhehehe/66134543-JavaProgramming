d4b7de145e2c298038c66101361aaf8abc91f1c0
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-19 21:11:18 +0700

    đã xong bài tách tên từ họ

--- LuyenTapThem_12_StreamvaFile_TachTenTuHo/src/tvu/runMain.java
+++ LuyenTapThem_12_StreamvaFile_TachTenTuHo/src/tvu/runMain.java
@@ -1,10 +1,23 @@
 package tvu;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class runMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		FileReader fr = new FileReader("HoTen.txt");
+		BufferedReader buf = new BufferedReader(fr);
+		while(true) {
+			String line = buf.readLine();
+			if(line == null ) 
+				break;
+			String[] s = line.split(" ");
+			System.out.println(s[s.length - 1]);
+		}
+		
 	}
 
 }
