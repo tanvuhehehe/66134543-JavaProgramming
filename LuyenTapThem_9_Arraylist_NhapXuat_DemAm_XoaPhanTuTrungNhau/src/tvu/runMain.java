99f2c324da32fb921aed6613681f90b28260e43c
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-19 11:26:47 +0700

    xong phương thức nhập n  +  tiếp nhé

--- LuyenTapThem_9_Arraylist_NhapXuat_DemAm_XoaPhanTuTrungNhau/src/tvu/runMain.java
+++ LuyenTapThem_9_Arraylist_NhapXuat_DemAm_XoaPhanTuTrungNhau/src/tvu/runMain.java
@@ -1,10 +1,23 @@
 package tvu;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class runMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//cau a: Nhap so nguyen n va n phan tu vao danh sach
+		ArrayList<Integer> ds = new ArrayList<Integer>();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Nhap so luong phan tu: ");
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print("a[" + i +"] = ");
+			Integer tam = sc.nextInt();
+			ds.add(tam);
+		}
 	}
 
 }
