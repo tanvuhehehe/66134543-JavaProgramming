6f7db646b8be9bde498ece8856ce1750dbb08feb
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-19 16:43:04 +0700

    đã xong học sinh 3 ngon luôn nhập từ bàn phiếm

--- LuyenTapThem_10_ArrayListkethopClass_BaiTapTrenLop_LamLai/src/tvu/runMain.java
+++ LuyenTapThem_10_ArrayListkethopClass_BaiTapTrenLop_LamLai/src/tvu/runMain.java
@@ -1,5 +1,7 @@
 package tvu;
 
+import java.util.Scanner;
+
 public class runMain {
 
 	public static void main(String[] args) {
@@ -14,6 +16,22 @@ public class runMain {
 		//hs2: constructor co tham so
 		HOCSINH hs2 = new HOCSINH("Mai Len Bang 2", 19.6, "Love2A");
 		System.out.println(hs2.toString());
+		 
+		//2b. DL nhap tu ban phim
+		Scanner sc = new Scanner(System.in);
+		HOCSINH hs3 = new HOCSINH();
+		System.out.print("- Nhap TenHS 3: ");
+		String tenhs3 = sc.nextLine();
+		System.out.print("- Nhap TuoiHS 3: ");
+		double tuoihs3 = sc.nextDouble();
+		sc.nextLine(); // xoa bo enter cua tuoihs
+		System.out.print("- Nhap LopHS 3: ");
+		String lophs3 = sc.nextLine();
+		// gan gia tri
+		hs3.setTenHS(tenhs3);
+		hs3.setTuoiHS(tuoihs3);
+		hs3.setLopHS(lophs3);
+		System.out.print(hs3.toString());
 	}
 
 }
