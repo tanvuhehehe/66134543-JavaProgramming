d34e89de19d6719c5beedc6922b553d235f8b101
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-19 16:21:45 +0700

    học sinh 1 đang tạo

--- LuyenTapThem_10_ArrayListkethopClass_BaiTapTrenLop_LamLai/src/tvu/runMain.java
+++ LuyenTapThem_10_ArrayListkethopClass_BaiTapTrenLop_LamLai/src/tvu/runMain.java
@@ -3,8 +3,12 @@ package tvu;
 public class runMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//2a. Tao moi 2 HS in ra man hinh
+		HOCSINH hs1 = new HOCSINH();
+		hs1.setTenHS("Mai Len Bang");
+		hs1.setTuoiHS(19.5);
+		hs1.setLopHS("Lop You");
+		System.out.println(hs1.toString());
 	}
 
 }
