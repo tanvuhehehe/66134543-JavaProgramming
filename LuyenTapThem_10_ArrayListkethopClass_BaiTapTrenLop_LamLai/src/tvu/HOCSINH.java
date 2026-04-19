bb6a2ff9155977d2bd12fd1fddc2a91d06da052f
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-19 16:02:37 +0700

    xong lớp getter + setter

--- LuyenTapThem_10_ArrayListkethopClass_BaiTapTrenLop_LamLai/src/tvu/HOCSINH.java
+++ LuyenTapThem_10_ArrayListkethopClass_BaiTapTrenLop_LamLai/src/tvu/HOCSINH.java
@@ -1,5 +1,30 @@
 package tvu;
 
 public class HOCSINH {
-
+	private String tenHS;
+	private double tuoiHS;
+	private String lopHS;
+	
+	//getter
+	public String getTenHS() {
+		return tenHS;
+	}
+	public double getTuoiHS() {
+		return tuoiHS;
+	}
+	public String getLopHS() {
+		return lopHS;
+	}
+	
+	//setter
+	public void setTenHS(String tenHS) {
+		this.tenHS = tenHS;
+	}
+	public void setTuoiHS(double tuoiHS) {
+		this.tuoiHS = tuoiHS;
+	}
+	public void setLopHS(String lopHS) {
+		this.lopHS = lopHS;
+	}
+	
 }
