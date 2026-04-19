5b9c6a40759c8f23672a1b6b49f3d8ac0b0497f9
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-19 11:33:30 +0700

    đếm số âm cách 1 nè thầy iu

--- LuyenTapThem_9_Arraylist_NhapXuat_DemAm_XoaPhanTuTrungNhau/src/tvu/runMain.java
+++ LuyenTapThem_9_Arraylist_NhapXuat_DemAm_XoaPhanTuTrungNhau/src/tvu/runMain.java
@@ -10,14 +10,45 @@ public class runMain {
 		ArrayList<Integer> ds = new ArrayList<Integer>();
 		Scanner sc = new Scanner(System.in);
 		
+		// nhap n: 
 		System.out.print("Nhap so luong phan tu: ");
 		int n = sc.nextInt();
 		
+		// nhap mang: 
 		for(int i = 0; i < n; i++) {
 			System.out.print("a[" + i +"] = ");
 			Integer tam = sc.nextInt();
 			ds.add(tam);
 		}
+		
+		
+		//cau b: Dem co bao nhieu so am trong danh sach
+		//cach dem 1: 
+		int cnt1 = 0;
+		for(int i = 0; i < n; i++) {
+			Integer tam = ds.get(i);
+			if(tam < 0) cnt1++;
+		}
+		System.out.println("- Dem am: " + cnt1);
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
