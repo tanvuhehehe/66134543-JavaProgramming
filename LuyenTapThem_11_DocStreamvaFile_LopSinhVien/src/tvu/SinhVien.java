3568efb15efc75e01ec59ac3166ca59f090d01fd
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-19 20:20:47 +0700

    đã tạo xong hàm sinh viên

--- LuyenTapThem_11_DocStreamvaFile_LopSinhVien/src/tvu/SinhVien.java
+++ LuyenTapThem_11_DocStreamvaFile_LopSinhVien/src/tvu/SinhVien.java
@@ -1,5 +1,63 @@
 package tvu;
 
 public class SinhVien {
+	private int STT;
+	private String hoTen;
+	private int namSinh;
+	private String gioiTinh;
+	
+	//constructor no tham so
+	public SinhVien() {
+		
+	}
+	
+	// constructor  co tham so
+	public SinhVien(int 	STT, String hoTen, int namSinh, String gioiTinh) {
+		super();
+		this.STT = STT;
+		this.hoTen = hoTen;
+		this.namSinh = namSinh;
+		this.gioiTinh = gioiTinh;
+	}
+	
 
+	
+	// getter va setter
+	public int getSTT() {
+		return STT;
+	}
+
+	public void setSTT(int sTT) {
+		STT = sTT;
+	}
+
+	public String getHoTen() {
+		return hoTen;
+	}
+
+	public void setHoTen(String hoTen) {
+		this.hoTen = hoTen;
+	}
+
+	public int getNamSinh() {
+		return namSinh;
+	}
+
+	public void setNamSinh(int namSinh) {
+		this.namSinh = namSinh;
+	}
+
+	public String getGioiTinh() {
+		return gioiTinh;
+	}
+
+	public void setGioiTinh(String gioiTinh) {
+		this.gioiTinh = gioiTinh;
+	}
+	//TOSTRING
+	@Override
+	public String toString() {
+		
+		return "SinhVien [STT=" + STT + ", hoTen=" + hoTen + ", namSinh=" + namSinh + ", gioiTinh=" + gioiTinh + "]";
+	}
 }
