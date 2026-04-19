eb33fadbf58646a9c182cab3f084390427d0e257
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-19 20:44:34 +0700

    dang lam

--- LuyenTapThem_11_DocStreamvaFile_LopSinhVien/src/tvu/runMain.java
+++ LuyenTapThem_11_DocStreamvaFile_LopSinhVien/src/tvu/runMain.java
@@ -1,8 +1,24 @@
 package tvu;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class runMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		FileReader fR = new FileReader("SV.csv");
+		BufferedReader bufRead = new BufferedReader(fR);
+		ArrayList<SinhVien> ds = new ArrayList<SinhVien>();
+		
+		while(true) {
+			String line = bufRead.readLine();
+			if (line == null) 
+				break;
+			String[] s = line.split("; "); 
+			SinhVien sv1 = new SinhVien(Integer.parseInt(s[0]), s[1], Integer.parseInt(s[2]), s[3]);
+		}
 		
 	}
 
