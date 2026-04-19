8e9ef0c8aafa69749aa448f2a8c26f733b8802ef
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-19 09:51:18 +0700

    đang viết chức năng nhập từ bàn phím

---
+++ LuyenTapThem_8_OOP_Arraylist_TongMang/src/tvu/runMain.java
@@ -0,0 +1,19 @@
+package tvu;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class runMain {
+
+	public static void main(String[] args) {
+		 ArrayList<Integer> ds = new ArrayList<Integer>();
+		 Scanner sc = new Scanner(System.in);
+		 
+		for(int i = 0; i < 10; i++) {
+		 System.out.println("a[" + i + "]");
+		 Integer tam = sc.nextInt(0);
+		}
+		
+	}
+
+}
