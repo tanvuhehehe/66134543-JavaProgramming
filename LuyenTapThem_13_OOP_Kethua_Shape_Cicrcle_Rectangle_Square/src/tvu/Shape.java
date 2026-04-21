b2a80c149fc0d730c09717ec3009b9f1b02b5d46
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-21 19:18:37 +0700

    xong getter setter

--- LuyenTapThem_13_OOP_Kethua_Shape_Cicrcle_Rectangle_Square/src/tvu/Shape.java
+++ LuyenTapThem_13_OOP_Kethua_Shape_Cicrcle_Rectangle_Square/src/tvu/Shape.java
@@ -1,5 +1,23 @@
 package tvu;
 
 public class Shape {
-
+	private String color;
+	private boolean filled;
+	//constructor
+	
+	//getter và setter
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public boolean isFilled() {
+		return filled;
+	}
+	public void setFilled(boolean filled) {
+		this.filled = filled;
+	}
+	
+	
 }
