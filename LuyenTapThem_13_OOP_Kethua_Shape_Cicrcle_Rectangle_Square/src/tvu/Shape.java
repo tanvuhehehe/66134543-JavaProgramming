1afc442797dfaaa9ea928180818187e0aea5c548
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-21 19:20:30 +0700

    xong hàm Shape

--- LuyenTapThem_13_OOP_Kethua_Shape_Cicrcle_Rectangle_Square/src/tvu/Shape.java
+++ LuyenTapThem_13_OOP_Kethua_Shape_Cicrcle_Rectangle_Square/src/tvu/Shape.java
@@ -4,7 +4,14 @@ public class Shape {
 	private String color;
 	private boolean filled;
 	//constructor
-	
+	public Shape() {
+		super();
+	}
+	public Shape(String color, boolean filled) {
+		super();
+		this.color = color;
+		this.filled = filled;
+	}
 	//getter và setter
 	public String getColor() {
 		return color;
@@ -17,6 +24,10 @@ public class Shape {
 	}
 	public void setFilled(boolean filled) {
 		this.filled = filled;
+	}
+	@Override
+	public String toString() {
+		return "Shape [color=" + color + ", filled=" + filled + "]";
 	}
 	
 	
