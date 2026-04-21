6c8fba8f2b31a509a1501f2d1bbdd74633b96d92
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-21 19:56:16 +0700

    xong hàm Rectangle

--- LuyenTapThem_13_OOP_Kethua_Shape_Cicrcle_Rectangle_Square/src/tvu/Rectangle.java
+++ LuyenTapThem_13_OOP_Kethua_Shape_Cicrcle_Rectangle_Square/src/tvu/Rectangle.java
@@ -16,9 +16,34 @@ public class Rectangle extends Shape{
 		super(color, filled);
 		this.width = width;
 		this.length = length;
-		
 	}
-	
-	
+	//getter và setter
+	public double getWidth() {
+		return width;
+	}
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	public double getLength() {
+		return length;
+	}
+	public void setLength(double length) {
+		this.length = length;
+	}
+	//tospring
+	@Override
+	public String toString() {
+		return "Rectangle[" + super.toString() + ", width= " + width +", length= " + length + "]";
+	}
+
 }
 	
+
+
+
+
+
+
+
+
+
