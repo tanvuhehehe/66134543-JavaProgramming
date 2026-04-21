405242204acecf2a241c723f9aba9fa3abccd221
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-21 20:07:43 +0700

    square đang tạo và giờ đi ăn tối

--- LuyenTapThem_13_OOP_Kethua_Shape_Cicrcle_Rectangle_Square/src/tvu/Square.java
+++ LuyenTapThem_13_OOP_Kethua_Shape_Cicrcle_Rectangle_Square/src/tvu/Square.java
@@ -1,5 +1,18 @@
 package tvu;
 
-public class Square {
-
+public class Square extends Rectangle{
+	//constructor
+	public Square() {
+		
+	}
+	
+	public Square(double side) {
+		super(side, side);
+		
+	}
+	public Square(double side, String color, 
+			boolean filled) 
+	{
+		super(color, filled);
+	}
 }
