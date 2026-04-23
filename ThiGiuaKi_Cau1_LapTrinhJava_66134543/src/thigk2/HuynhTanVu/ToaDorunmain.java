327df670ca210750d23f2749f229e09df7d5c4b6
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-23 07:48:43 +0700

    nộp lần 4 + hoàn thành câu 1

--- ThiGiuaKi_Cau1_LapTrinhJava_66134543/src/thigk2/HuynhTanVu/ToaDorunmain.java
+++ ThiGiuaKi_Cau1_LapTrinhJava_66134543/src/thigk2/HuynhTanVu/ToaDorunmain.java
@@ -1,3 +1,15 @@
+	/*
+	 * ban đầu em khai báo biến của đề bài x1, y1 và x2, y2
+	 * sau đó em hỏi người dùng nhập 4 biến này
+	 * sau đó em xuất ra màn hình cái thông tin mà người dùng vừa nhập 
+	 * sau đó em đi tính chiều dài sẽ bằng trị tuyệt đối của tọa độ x2- x1
+	 * chiều rộng cũng vậy là y1-y2
+	 * sau đó em tính chu vi bằng cách chiều( dài + cộng chiều rộng) nhân 2 ( dài + rộng )x2
+	 * sau đó em tính diện tích là bằng (chiều dài x nhân chiều rộng ) (chiều dài x chiều rộng )
+	 * 
+	 * 
+	 * */
+
 package thigk2.HuynhTanVu;
 
 import java.util.Scanner;
@@ -31,5 +43,8 @@ public class ToaDorunmain {
 	        // tính chu vi và diện tích
 	        double chuVi = 2 * (chieuDai + chieuRong);
 	        double dienTich = chieuDai * chieuRong;
+	        
+	        System.out.println("Chu vi của hình chữ nhật là: " + chuVi);
+	        System.out.println("Dien tich của hình chữ nhật là: " + dienTich);
 	}
 }
