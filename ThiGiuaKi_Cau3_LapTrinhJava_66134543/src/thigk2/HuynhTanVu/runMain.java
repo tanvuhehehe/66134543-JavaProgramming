e3fa6f8b66c5b093a5e2839dc0d150cce8294d7b
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-23 08:32:02 +0700

    Lần thứ 12: đã làm

--- ThiGiuaKi_Cau3_LapTrinhJava_66134543/src/thigk2/HuynhTanVu/runMain.java
+++ ThiGiuaKi_Cau3_LapTrinhJava_66134543/src/thigk2/HuynhTanVu/runMain.java
@@ -1,54 +1,54 @@
+
+/*Đầu tiên em tạo danh sách ArrayList<Double> để lưu các số thực.
+Sau đó em sử dụng FileReader và BufferedReader để đọc dữ liệu từ file.
+Em đọc từng dòng trong file bằng vòng lặp while.
+Mỗi dòng em chuyển sang kiểu double bằng Double.parseDouble().
+Em thêm từng số vào danh sách.
+Sau khi đọc xong, em đóng file lại.
+Tiếp theo em duyệt danh sách và in các số ra màn hình.
+Em sử dụng Scanner để nhập giá trị X từ bàn phím.
+Em kiểm tra X có tồn tại trong danh sách bằng cách dùng contains hoặc duyệt.
+Cuối cùng em in ra kết quả X có hoặc không có trong danh sách.
+ * */
 package thigk2.HuynhTanVu;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class runMain {
 
-	public static void main(String[] args) {
-		 ArrayList<Double> ds = new ArrayList<>();
+	public static void main(String[] args) throws IOException {
+		ArrayList<Double> ds = new ArrayList<>();
 
-	        // Đọc file
-	        try {
-	            BufferedReader br = new BufferedReader(new FileReader("so.txt"));
-	            String line;
-	            
-	            while ((line = br.readLine()) != null) {
-	                double so = Double.parseDouble(line);
-	                ds.add(so);
-	            }
-	            br.close();
-	        } catch (Exception e) {
-	            System.out.println("Loi doc file!");
-	        }
+        // Đọc file
+        BufferedReader br = new BufferedReader(new FileReader("data.txt"));
+        String line;
 
-	        // In danh sách
-	        System.out.println("Danh sach cac so:");
-	        for (double x : ds) {
-	            System.out.println(x);
-	        }
+        while ((line = br.readLine()) != null) {
+            ds.add(Double.parseDouble(line));
+        }
+        br.close();
 
-	        // Nhap X (ngay sinh)
-	        Scanner sc = new Scanner(System.in);
-	        System.out.print("Nhap gia tri X: ");
-	        double X = sc.nextDouble();
+        // In danh sách
+        System.out.println("Danh sach cac so:");
+        for (double x : ds) {
+            System.out.println(x);
+        }
 
-	        // Kiem tra X co ton tai khong
-	        boolean timThay = false;
-	        for (double x : ds) {
-	            if (x == X) {
-	                timThay = true;
-	                break;
-	            }
-	        }
+        // Nhap X
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap X: ");
+        double X = sc.nextDouble();
 
-	        if (timThay) {
-	            System.out.println("X co trong danh sach");
-	        } else {
-	            System.out.println("X khong co trong danh sach");
-	        }
+        // Kiem tra X
+        if (ds.contains(X)) {
+            System.out.println("X co trong danh sach");
+        } else {
+            System.out.println("X khong co trong danh sach");
+        }
 
 	}
 
