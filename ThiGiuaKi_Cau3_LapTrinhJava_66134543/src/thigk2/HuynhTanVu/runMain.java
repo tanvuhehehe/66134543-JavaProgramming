6fea17037c646ab9e1b22a653645e1b11ae1fbe7
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-23 08:29:31 +0700

    Lần thứ 11

--- ThiGiuaKi_Cau3_LapTrinhJava_66134543/src/thigk2/HuynhTanVu/runMain.java
+++ ThiGiuaKi_Cau3_LapTrinhJava_66134543/src/thigk2/HuynhTanVu/runMain.java
@@ -1,10 +1,56 @@
 package thigk2.HuynhTanVu;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class runMain {
 
 	public static void main(String[] args) {
-		
+		 ArrayList<Double> ds = new ArrayList<>();
+
+	        // Đọc file
+	        try {
+	            BufferedReader br = new BufferedReader(new FileReader("so.txt"));
+	            String line;
+	            
+	            while ((line = br.readLine()) != null) {
+	                double so = Double.parseDouble(line);
+	                ds.add(so);
+	            }
+	            br.close();
+	        } catch (Exception e) {
+	            System.out.println("Loi doc file!");
+	        }
+
+	        // In danh sách
+	        System.out.println("Danh sach cac so:");
+	        for (double x : ds) {
+	            System.out.println(x);
+	        }
+
+	        // Nhap X (ngay sinh)
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Nhap gia tri X: ");
+	        double X = sc.nextDouble();
+
+	        // Kiem tra X co ton tai khong
+	        boolean timThay = false;
+	        for (double x : ds) {
+	            if (x == X) {
+	                timThay = true;
+	                break;
+	            }
+	        }
+
+	        if (timThay) {
+	            System.out.println("X co trong danh sach");
+	        } else {
+	            System.out.println("X khong co trong danh sach");
+	        }
 
 	}
 
 }
+	
