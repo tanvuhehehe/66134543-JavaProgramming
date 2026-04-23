455a53c87558828a3a20f31611af0e1f0eab4167
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-23 07:55:11 +0700

    nộp lần 6 +  tạo xong các phương thức thầy yêu cầu

--- ThiGiuaKi_Cau2_LapTrinhJava_66134543/src/thigk2/HuynhTanVu/SanPham.java
+++ ThiGiuaKi_Cau2_LapTrinhJava_66134543/src/thigk2/HuynhTanVu/SanPham.java
@@ -1,5 +1,52 @@
 package thigk2.HuynhTanVu;
 
 public class SanPham {
+	private String maSP;
+	private String tenSP;
+	private String loaiSP;
+	private int soLuong; // thuộc tính thêm
+	
+	// contructor
+	public SanPham() {
+		
+	}
+	
+	// contructor có tham số
+	public SanPham(String maSP, String tenSP, String loaiSP, int soLuong) {
+		super();
+		this.maSP = maSP;
+		this.tenSP = tenSP;
+		this.loaiSP = loaiSP;
+		this.soLuong = soLuong;
+	}
 
+
+	//getter setter
+	public String getMaSP() {
+		return maSP;
+	}
+	public void setMaSP(String maSP) {
+		this.maSP = maSP;
+	}
+	public String getTenSP() {
+		return tenSP;
+	}
+	public void setTenSP(String tenSP) {
+		this.tenSP = tenSP;
+	}
+	public String getLoaiSP() {
+		return loaiSP;
+	}
+	public void setLoaiSP(String loaiSP) {
+		this.loaiSP = loaiSP;
+	}
+	public int getSoLuong() {
+		return soLuong;
+	}
+	public void setSoLuong(int soLuong) {
+		this.soLuong = soLuong;
+	}
+
+	
+	
 }
