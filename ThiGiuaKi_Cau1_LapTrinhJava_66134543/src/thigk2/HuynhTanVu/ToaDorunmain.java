ad0f838c8f772f178806f811a2bbcda725e96b0a
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-23 07:42:20 +0700

    nộp lần 3 + tính chiều dài chiều rộng

--- ThiGiuaKi_Cau1_LapTrinhJava_66134543/src/thigk2/HuynhTanVu/ToaDorunmain.java
+++ ThiGiuaKi_Cau1_LapTrinhJava_66134543/src/thigk2/HuynhTanVu/ToaDorunmain.java
@@ -7,7 +7,7 @@ public class ToaDorunmain {
 	public static void main(String[] args) {
 		 	Scanner sc = new Scanner(System.in);
 
-	        double x1, y1, x2, y2;
+	        double x1, y1, x2, y2; // tạo toạ độ trên trái là x1, y1 và dưới phải x2, y2
 
 	        System.out.print("Nhap x1 (goc tren trai): ");
 	        x1 = sc.nextDouble();
@@ -20,7 +20,16 @@ public class ToaDorunmain {
 
 	        System.out.print("Nhap y2 (goc duoi phai): ");
 	        y2 = sc.nextDouble();
+	        
+	        System.out.println("Toa do hinh chu nhat:");
+	        System.out.println("Goc tren trai: (" + x1 + ", " + y1 + ")");
+	        System.out.println("Goc duoi phai: (" + x2 + ", " + y2 + ")");
+	        
+	        double chieuDai = Math.abs(x2 - x1);
+	        double chieuRong = Math.abs(y1 - y2);
 
-		
+	        // tính chu vi và diện tích
+	        double chuVi = 2 * (chieuDai + chieuRong);
+	        double dienTich = chieuDai * chieuRong;
 	}
 }
