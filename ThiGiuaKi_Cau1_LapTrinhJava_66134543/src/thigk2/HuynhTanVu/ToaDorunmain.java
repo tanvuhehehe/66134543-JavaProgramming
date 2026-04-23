0ac57f9666da83d930531eb163021ffef47f40d9
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-23 07:39:12 +0700

    Nộp lần 2

---
+++ ThiGiuaKi_Cau1_LapTrinhJava_66134543/src/thigk2/HuynhTanVu/ToaDorunmain.java
@@ -0,0 +1,26 @@
+package thigk2.HuynhTanVu;
+
+import java.util.Scanner;
+
+public class ToaDorunmain {
+
+	public static void main(String[] args) {
+		 	Scanner sc = new Scanner(System.in);
+
+	        double x1, y1, x2, y2;
+
+	        System.out.print("Nhap x1 (goc tren trai): ");
+	        x1 = sc.nextDouble();
+
+	        System.out.print("Nhap y1 (goc tren trai): ");
+	        y1 = sc.nextDouble();
+
+	        System.out.print("Nhap x2 (goc duoi phai): ");
+	        x2 = sc.nextDouble();
+
+	        System.out.print("Nhap y2 (goc duoi phai): ");
+	        y2 = sc.nextDouble();
+
+		
+	}
+}
--- ThiGiuaKi_LapTrinhJava_66134543/src/thigk2/HuynhTanVu/ToaDo.java
+++
@@ -1,5 +0,0 @@
-package thigk2.HuynhTanVu;
-
-public class ToaDo {
-
-}
