8ce631c284509aa09335a2ba08ad4f51163bba33
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-04-23 08:06:07 +0700

    NỘP LẦN THỨ 7 + XONG CÂU B BÀI 2

--- ThiGiuaKi_Cau2_LapTrinhJava_66134543/src/thigk2/HuynhTanVu/runMain.java
+++ ThiGiuaKi_Cau2_LapTrinhJava_66134543/src/thigk2/HuynhTanVu/runMain.java
@@ -1,6 +1,7 @@
 package thigk2.HuynhTanVu;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class runMain {
 
@@ -8,15 +9,41 @@ public class runMain {
 		// Hardcode 3 sản phẩm
 		// cau a: 
 		ArrayList<SanPham> ds = new ArrayList<>();
-		ds.add(new SanPham("SP01", "Ban phim co RGB Cuc Manh", "Thiet bi gaming", 10));
-		ds.add(new SanPham("SP02", "Chuot Khong Day Bay Nhu Gio", "Thiet bi gaming", 15));
-		ds.add(new SanPham("SP03", "Man hinh 240Hz Nhin La Dinh", "Thiet bi gaming", 5));
+		SanPham sanPham1 = new SanPham("SP01", "Ban phim Go La Chay Deadline", "Thiet bi", 10);
+		SanPham sanPham2 = new SanPham("SP02", "Chuot Click 1 Cai Len Rank", "Thiet bi", 15);
+		SanPham sanPham3 = new SanPham("SP03", "Man hinh Nhin La Het Buon Ngu", "Thiet bi", 5);
+
+		// thêm vào danh sách
+		ds.add(sanPham1);
+		ds.add(sanPham2);
+		ds.add(sanPham3);
+		
         // In danh sách ban đầu
         System.out.println("Danh sach san pham ban dau:");
         for (SanPham sp : ds) {
             System.out.println(sp);
         }
+        // cau b: 
         
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nNhap pham moi cua cau b:");
+        System.out.print("Ma SP: ");
+        String ma = sc.nextLine();
+
+        System.out.print("Ten SP: ");
+        String ten = sc.nextLine();
+
+        System.out.print("Loai SP: ");
+        String loai = sc.nextLine();
+
+        System.out.print("So luong: ");
+        int sl = sc.nextInt();
+
+        // tạo thêm đối tượng thêm yêu cầu 
+        SanPham sanPham4 = new SanPham(ma, ten, loai, sl);
+        // xuất sản phẩm 4
+        System.out.println("San Pham 4: " + sanPham4.toString());
 	}
 
 }
