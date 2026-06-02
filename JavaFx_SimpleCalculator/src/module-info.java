fd9b0cfa04af3f036660073b75b2d0eb35da788f
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-06-02 14:37:25 +0700

    tạo dự án mới

---
+++ JavaFx_SimpleCalculator/src/module-info.java
@@ -0,0 +1,5 @@
+module JavaFx_SimpleCalculator {
+	requires javafx.controls;
+	
+	opens tanvu.app to javafx.graphics, javafx.fxml;
+}
---
+++ QuanLySieuThiMini/src/module-info.java
@@ -0,0 +1,5 @@
+module QuanLySieuThiMini {
+	requires javafx.controls;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
